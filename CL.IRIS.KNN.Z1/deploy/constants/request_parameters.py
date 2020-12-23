cf24cb2b8ab0d72c2b654bc58d69b9a99957b9ac
Mohamed Sabri <mohamed@datalyticsbusiness.com>
2020-12-23 17:17:08 -0500

    final

--- CL.IRIS.KNN.Z1/deploy/constants/request_parameters.py
+++
@@ -1,5 +0,0 @@
-class RequestParameters:
-    VAR1 = "VAR1"
-    VAR2 = "VAR2"
-    VAR3 = "VAR3"
-    VAR4 = "VAR4"
--- CL.IRIS.KNN.Z1/seldon/Iris.py
+++ CL.IRIS.KNN.Z1/seldon/Iris.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 from typing import Dict, List
-from sklearn.externals import joblib
+import joblib
 from sklearn.neighbors import KNeighborsClassifier as KNN
-from deploy.constants.request_parameters import RequestParameters
+from sklearn.neighbors import NearestNeighbors
 
 
 class Iris(object):
@@ -12,22 +12,11 @@ class Iris(object):
     def __init__(self):
         self.knn_model = joblib.load('iris_model1.pkl')
         
-    def predict(self, request: Dict, features_names=None) -> Dict:
-        VAR1 = request[RequestParameters.VAR1]
-        VAR2 = request[RequestParameters.VAR2]
-        VAR3 = request[RequestParameters.VAR3]
-        VAR4 = request[RequestParameters.VAR4]
+    def predict(self,X,features_names):
+    
+        x_score = np.array(X)
         
-        x_score = np.array([VAR1,VAR2,VAR3,VAR4])
-        
-        output1 = self.knn_model.predict(x_score)
+        output1 = self.knn_model.predict(x_score)[0]
 
-        return self.make_response(x_score,output1)
+        return np.array(output1)
 
-    def make_response(self, input1: List, output1: List) -> Dict:
-        response = []
-        item = {
-                "Input": input1, "Class": output1}
-        response.append(item)
-
-        return response
